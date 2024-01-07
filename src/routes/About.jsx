f20ae2ddfc6156233deebf022c929109479d7f58
Auden Woolfson <auden.woolfson@gmail.com>
2024-01-06 20:39:31 -0500

    added real writing

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -40,9 +40,16 @@ const About = () => {
             nice to meet you!
           </h2>
           <p className="about-text-p">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam maiores reiciendis repellat, 
-            non totam porro doloremque illum suscipit odit dolore 
-            repellendus necessitatibus voluptates soluta officiis magni neque blanditiis distinctio? Unde!
+          My name is Auden Woolfson, and I’m from West Hartford, CT.
+
+I graduated from Conard High School in 2019, and from there I began studying computer science at Connecticut College. One project I had the opportunity to take on during my time in college was an algorithm that matched students with course sections for registration. The algorithm was able to replace the first come first serve system that had been used in the computer science department for years, providing greater ease to both students and professors in the registration process and new insights into which courses students preferred. I also conducted bioinformatics research, which was an incredible way for me to combine my interests in biology and environmental studies with my technological skills.
+
+When I'm not studying or working, I love to get outdoors. In 2021, I through-hiked the Appalachian Trail, and I've been exploring bits of the Long Trail in Vermont since then. The call of the wild out west is getting louder, and I've got my sights set on the Pacific Crest Trail someday.
+
+Music is a huge part of my life, and I'm always practicing guitar and drums. I've been jamming on both instruments with cover bands at local venues for a couple of years now, and it's a blast.
+
+And, of course, there's my digital playground—computer programming. I love diving into new tech and cooking up projects that spark my interest whenever I've got some downtime.
+
           </p>
         </div>
       </div>
